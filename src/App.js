ad0f1c31952814f94728c82f72681437a0243ca6
Junyaaa <bb.junjun2@gmail.com>
2023-04-10 20:06:25 +0900

    staion3-1

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ export const App = () => {
   return (
     <div>
       <header>trail</header>
+      <body>
+        <img src="https://images.dog.ceo/breeds/puggle/IMG_151824.jpg" alt="犬の写真です" />
+
+      </body>
     </div>
   )
 }
+
