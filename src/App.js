b94bf33b93d5e8b26785f4bc77e680a9702e92a0
Junyaaa <bb.junjun2@gmail.com>
2023-04-11 23:51:32 +0900

    station4-24

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 export const App = () => {
   // const dogUrl : any;
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/puggle/IMG_151824.jpg");
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/wolfhound-irish/n02090721_6051.jpg");
 }
 
 function Dog() {
@@ -11,7 +11,8 @@ function Dog() {
     <div>
       <header>trail</header>
       <body>
-        <img src={dogUrl} alt="犬の写真です" />
+        <p>https://images.dog.ceo/breeds/wolfhound-irish/n02090721_6051.jpg</p>
+        <img src={dogUrl}/>
       </body>
     </div>
   );
