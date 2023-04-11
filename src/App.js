7504a28ac591978ae42131fe12522ed4a07602bb
Junyaaa <bb.junjun2@gmail.com>
2023-04-11 21:21:31 +0900

    staion4-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // DO NOT DELETE
 
-import * as React from 'react'
-import './App.css'
+// import * as React from 'react'
+// import './App.css'
 import React, { useState } from 'react';
 
 const App = () => {
