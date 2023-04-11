c781fb286a8cd0e605503458edba071872cb46f0
Junyaaa <bb.junjun2@gmail.com>
2023-04-11 21:26:49 +0900

    staion4-4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // DO NOT DELETE
 
-// import * as React from 'react'
-// import './App.css'
+import * as React from 'react'
+import './App.css'
 import React, { useState } from 'react';
 
-const App = () => {
+export const App = () => {
   // stateを定義
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/puggle/IMG_151824.jpg');
+  function dog(){
+    const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/puggle/IMG_151824.jpg');
 
-  return (
-    <div>
-      <header>trail</header>
-      <body>
-        <img src={dogUrl} alt="犬の写真です" />
-      </body>
-    </div>
-  );
+    return (
+      <div>
+        <header>trail</header>
+        <body>
+          <img src={dogUrl} alt="犬の写真です" />
+        </body>
+      </div>
+    );
+  } 
 }
-
-export default App;
