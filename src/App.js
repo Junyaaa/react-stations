0e2a652c2751ecad508a338db6abc5284217adba
Junyaaa <bb.junjun2@gmail.com>
2023-04-12 00:45:25 +0900

    station4-39

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/wolfhound-irish/n02090721_6051.jpg");
+  const dogUrl = useState("https://images.dog.ceo/breeds/wolfhound-irish/n02090721_6051.jpg");
 }
 
 function Dog() {
