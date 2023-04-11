5ac641783748f616c5bc179016ccf406460a648b
Junyaaa <bb.junjun2@gmail.com>
2023-04-11 23:42:12 +0900

    station4-21

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './app.css'
 import React, { useState } from 'react'
 
 export const App = () => {
+  // const dogUrl = ""
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/puggle/IMG_151824.jpg');
 }
 
@@ -10,7 +11,7 @@ function Dog() {
     <div>
       <header>trail</header>
       <body>
-        <img src={dogUrl} alt="犬の写真です" />
+        <img src={this.state.dogUrl} alt="犬の写真です" />
       </body>
     </div>
   );
