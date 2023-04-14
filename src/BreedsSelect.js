291b4a949ee519e624832ad78f820b892ca63070
Junyaaa <bb.junjun2@gmail.com>
2023-04-14 23:01:45 +0900

    station10-5

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 function BreedsSelect({ breeds, selectedBreed, handleBreedChange }) {
-  return (
-    <select value={selectedBreed} onChange={handleBreedChange}>
-      <option value=''>Select a breed</option>
-      {breeds.map((breed) => (
-        <option key={breed} value={breed}>
-          {breed}
-        </option>
-      ))}
-    </select>
-  );
+return (
+<select value={selectedBreed} onChange={handleBreedChange}>
+<option value=''>Select a breed</option>
+{breeds.map((breed) => (
+<option key={breed} value={breed}>
+{breed}
+</option>
+))}
+</select>
+);
 }
 
 export default BreedsSelect;
